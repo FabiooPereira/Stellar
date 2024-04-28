c1b8510bf6dd4d946afc85f8249f2d1a6054f9c1
MarcusZ98 <marcus.zibung@gmail.com>
2024-04-29 00:30:19 +0200

    Stella C++

--- Source/GruppOnion/Character_Stella.h
+++ Source/GruppOnion/Character_Stella.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GruppOnionCharacter.h"
+#include "Components/SphereComponent.h"
 #include "Character_Stella.generated.h"
 
 /**
@@ -13,5 +14,31 @@ UCLASS()
 class GRUPPONION_API ACharacter_Stella : public AGruppOnionCharacter
 {
 	GENERATED_BODY()
+
+public:
+	ACharacter_Stella();
 	
+	void InteractWithObject(AActor* Other);
+	void Disableinteract(AActor* Other);
+	void SetInteractable(AActor* Other);
+	void LockPlayerMovement();
+	void UnlockPlayerMovement();
+
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Collision")
+	USphereComponent* InteractionSphere;
+
+private:
+	UPROPERTY()
+	AActor* InteractableObject;
+
 };
+
+
