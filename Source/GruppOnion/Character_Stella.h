b2d1f0f947bab6fcb2b335a05f055bec96e2252e
MarcusZ98 <marcus.zibung@gmail.com>
2024-04-28 16:29:17 +0200

    Created Character_Stella, changed parents

---
+++ Source/GruppOnion/Character_Stella.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GruppOnionCharacter.h"
+#include "Character_Stella.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRUPPONION_API ACharacter_Stella : public AGruppOnionCharacter
+{
+	GENERATED_BODY()
+	
+};
